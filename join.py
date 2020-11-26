b2800459dc57177019b1de1857044028846e67d8
David Ohayon <davideytanohayon@gmail.com>
2020-11-26 23:59:08 +0200

    finale update (I think)

--- join.py
+++ join.py
@@ -273,6 +273,8 @@ if __name__ == "__main__":
 
     print(schedule())
 
+    sleep(7)
+
     if not which_lesson(True):
         print("there's no class right now.\n")
         quit()
--- lessons.py
+++ lessons.py
@@ -5,10 +5,9 @@ torah_leeson_link_dov = 'https://edu-il.zoom.us/wc/join/3935560602'
 torah_leeson_link_rabinak = 'https://edu-il.zoom.us/wc/join/3422464676?pwd=aUlOWFFjUEx0ak5yalFpTjBHdU1Xdz09'
 programming_lesson_link = 'https://edu-il.zoom.us/wc/join/7184928368?pwd=cmZ5Sm9BdXdtWEcrcFBWN0pRRlZjUT09'
 math_lesson_link = 'https://edu-il.zoom.us/wc/join/3198379053?pwd=elcrU1Nxb3hpaGNOQzVBNzhiOHR4Zz09'
-physics_lesson_link = 'https://zoom.us/wc/join/98729071010?pwd=ajVBbU13bHBqdmhCemlqQWV2aEZFUT09'
+physics_lesson_link = 'https://zoom.us/wc/join/96831626748?pwd=Q1ltKzljdmF2RVVHRDZGREFQdmNIZz09'
 english_lesson_link = 'https://edu-il.zoom.us/wc/join/6259151077'
 history_lesson_link_itsik = 'https://edu-il.zoom.us/wc/join/3136023920'
-# history_lesson_link_tsipi = 'https://chperber.my.webex.com/meet/chperber'
 hebrew_lesson_link = 'https://us04web.zoom.us/wc/join/77765386769?pwd=UE51NmNjL2Y1elNlMW0rcXJyUkNWZz09'
 literature_lesson_link = 'https://edu-il.zoom.us/wc/join/9655244107?pwd=c1h5RGNyYUs5QndDUHpqTFMyUmFqUT09'
 
@@ -24,7 +23,7 @@ def schedule():
     if sunday:
         return '''\n08:45-10:15: hebrew
 10:30-12:00: gemara
-12:30-14:00: math
+13:00-14:00: math
 14:30-16:00: english
 16:15-17:45: programming
 17:55-18:40: programming\n'''
@@ -36,9 +35,9 @@ def schedule():
 13:15-14:00: english
 18:30-19:30: programming\n'''
     elif tuesday:
-        return '''\n08:45-10:15: history tsipi
-10:30-11:15: torah rabinak
-11:15-12:00: history itsik
+        return '''\n08:45-9:30: history tsipi
+9:30-10:15: torah rabinak
+10:30-11:15: history itsik
 12:30-14:00: physics
 14:30-16:00: physics\n'''
     elif wednesday:
@@ -48,7 +47,7 @@ def schedule():
 12:30-14:00: programming
 14:30-16:00: programming\n'''
     elif thursday:
-        return '''\n08:45-10:15: gemara
+        return '''\n09:30-10:15: gemara
 10:30-11:15: gemara
 12:30-14:00: physics
 14:30-16:00: physics\n'''
@@ -60,32 +59,36 @@ def which_lesson(lesson):
     hour_min = datetime.now().strftime('%H:%M')
 
     lessons = {
+        # sunday
         sunday and hour_min >= '08:42' and hour_min <= '10:15': hebrew_lesson_link,
         sunday and hour_min >= '10:27' and hour_min <= '12:00': gemara_lesson_link,
-        sunday and hour_min >= '12:27' and hour_min <= '14:00': math_lesson_link,
+        sunday and hour_min >= '12:57' and hour_min <= '14:00': math_lesson_link,
         sunday and hour_min >= '14:27' and hour_min <= '16:00': english_lesson_link,
         sunday and hour_min >= '16:12' and hour_min <= '17:45': programming_lesson_link,
         sunday and hour_min >= '17:52' and hour_min <= '18:40': programming_lesson_link,
 
+        # monday
         monday and hour_min >= '08:42' and hour_min <= '10:15': gemara_lesson_link,
         monday and hour_min >= '10:27' and hour_min <= '11:15': history_lesson_link_itsik,
         monday and hour_min > '11:15' and hour_min <= '12:00': literature_lesson_link,
         monday and hour_min >= '12:27' and hour_min < '13:15': torah_leeson_link_dov,
         monday and hour_min >= '13:15' and hour_min <= '14:00': english_lesson_link,
 
-        # tuesday and hour_min >= '08:42' and hour_min <= '10:15': history_lesson_link_tsipi,
-        tuesday and hour_min >= '10:27' and hour_min < '11:15': torah_leeson_link_rabinak,
-        tuesday and hour_min >= '11:15' and hour_min <= '12:00': history_lesson_link_itsik,
+        # tuesday
+        tuesday and hour_min >= '9:27' and hour_min < '10:15': torah_leeson_link_rabinak,
+        tuesday and hour_min >= '10:27' and hour_min <= '11:15': history_lesson_link_itsik,
         tuesday and hour_min >= '12:27' and hour_min <= '14:00': physics_lesson_link,
         tuesday and hour_min >= '14:27' and hour_min <= '16:00': physics_lesson_link,
 
+        # wednesday
         wednesday and hour_min >= '08:42' and hour_min <= '10:15': math_lesson_link,
         wednesday and hour_min >= '10:27' and hour_min < '11:15': literature_lesson_link,
         wednesday and hour_min >= '11:15' and hour_min <= '12:00': gemara_lesson_link,
         wednesday and hour_min >= '12:27' and hour_min <= '14:00': programming_lesson_link,
         wednesday and hour_min >= '14:27' and hour_min <= '16:00': programming_lesson_link,
 
-        thursday and hour_min >= '08:42' and hour_min <= '10:15': gemara_lesson_link,
+        # thursday
+        thursday and hour_min >= '09:27' and hour_min <= '10:15': gemara_lesson_link,
         thursday and hour_min >= '10:27' and hour_min <= '11:15': gemara_lesson_link,
         thursday and hour_min >= '12:27' and hour_min <= '14:00': physics_lesson_link,
         thursday and hour_min >= '14:27' and hour_min <= '16:00': physics_lesson_link,
